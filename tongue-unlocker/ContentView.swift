85d3e182ca787bbcb62a34181731f35ada9f30cc
brenorios-br2digital <rbrenorios@gmail.com>
2023-07-22 08:30:45 +0100

    Object recognition view working

--- tongue-unlocker/ContentView.swift
+++ tongue-unlocker/ContentView.swift
@@ -7,74 +7,111 @@
 
 import SwiftUI
 import CoreData
+import AVKit
+import Vision
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
+//    @Environment(\.managedObjectContext) private var viewContext
+    
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+//    @FetchRequest(
+//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+//        animation: .default)
+//    private var items: FetchedResults<Item>
+//
+//    var body: some View {
+//        NavigationView {
+//            List {
+//                ForEach(items) { item in
+//                    NavigationLink {
+//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+//                    } label: {
+//                        Text(item.timestamp!, formatter: itemFormatter)
+//                    }
+//                }
+//                .onDelete(perform: deleteItems)
+//            }
+//            .toolbar {
+//#if os(iOS)
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    EditButton()
+//                }
+//#endif
+//                ToolbarItem {
+//                    Button(action: addItem) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
+//            Text("Select an item")
+//        }.onAppear {
+//
+//        }
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+        
+        var body: some View {
+            VStack {
+                
+                SwiftUIAVCaptureVideoPreviewView()
+                    .ignoresSafeArea(.all, edges: .all)
+                
+                Button(action: {
+                    isOnboardingViewActive = true
+                }) {
+                    
+                    // the image and the text are in the button label, then these are automatically horizontally aligned.
+                    // so don't need to wrap them in hstack.
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                        .imageScale(.large)
+                    
+                    Text("Restart")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+                //: Button
+                
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            
         }
     }
+    
+    
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(context: viewContext)
+//            newItem.timestamp = Date()
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+//    private func deleteItems(offsets: IndexSet) {
+//        withAnimation {
+//            offsets.map { items[$0] }.forEach(viewContext.delete)
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-}
 
 private let itemFormatter: DateFormatter = {
     let formatter = DateFormatter()
---
+++ tongue-unlocker/SwiftUIAVCaptureVideoPreviewView.swift
@@ -0,0 +1,128 @@
+//
+//  NewObjectRecognition:Views:SwiftUIAVCaptureVideoPreviewView.swift
+//  tongue-unlocker
+//
+//  Created by Breno Rios on 22/07/2023.
+//
+
+import SwiftUI
+import AVFoundation
+import Vision
+
+class UIAVCaptureVideoPreviewView: UIView, AVCaptureVideoDataOutputSampleBufferDelegate {
+    // AVCaptureVideoDataOutputSampleBufferDelegate
+    // when you want to handle the video input every second, you will add the delegate
+    var recognitionInterval = 0 //Interval for object recognition
+    
+    var mlModel: VNCoreMLModel? // CoreML model
+    
+    var captureSession: AVCaptureSession!
+    
+    var resultLabel: UILabel!
+    
+    func setModel() {
+        
+        // create instance, model property
+        mlModel = try? VNCoreMLModel(for: MobileNetV2().model)
+    }
+    
+    func setupSession() {
+        captureSession = AVCaptureSession()
+        captureSession.beginConfiguration()
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        guard let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice) else { return }
+        guard captureSession.canAddInput(videoInput) else { return }
+        captureSession.addInput(videoInput)
+        
+        // Output settings
+        let output = AVCaptureVideoDataOutput()
+        output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoQueue")) // set delegate to receive the data every frame
+        if captureSession.canAddOutput(output) {
+            captureSession.addOutput(output)
+        }
+        
+        captureSession.commitConfiguration()
+    }
+    
+    func setupPreview() {
+        self.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        previewLayer.frame = self.frame
+        
+        self.layer.addSublayer(previewLayer)
+        
+        
+        resultLabel = UILabel()
+        resultLabel.text = ""
+        resultLabel.frame = CGRect(x: 0, y: UIScreen.main.bounds.height - 200, width: UIScreen.main.bounds.width, height: 80)
+        resultLabel.textColor = UIColor.black
+        resultLabel.textAlignment = NSTextAlignment.center
+        resultLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        resultLabel.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.7)
+        
+        self.addSubview(resultLabel)
+        
+        
+        DispatchQueue.global(qos: .background).async {
+                self.captureSession.startRunning()
+            }
+    }
+    
+    // captureOutput will be called for each frame was written
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        // Recognise the object every 20 frames
+        if recognitionInterval < 20 {
+            recognitionInterval += 1
+            return
+        }
+        recognitionInterval = 0
+        
+        
+        // Convert CMSampleBuffer(an object holding media data) to CMSampleBufferGetImageBuffer
+        guard
+            let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer),
+            let model = mlModel // Unwrap the mlModel
+        else { return }
+        
+        // Create image process request, pass model and result
+        let request = VNCoreMLRequest(model: model) { //An image analysis request that uses a Core ML model to process images.
+            
+            (request: VNRequest, error: Error?) in
+            
+            // Get results as VNClassificationObservation array
+            guard let results = request.results as? [VNClassificationObservation] else { return }
+            
+            // top 5 results
+            var displayText = ""
+            for result in results.prefix(5) {
+                displayText += "\(Int(result.confidence * 100))%" + result.identifier + "\n"
+            }
+            
+            print(displayText)
+            // Execute it in the main thread
+            DispatchQueue.main.async {
+                self.resultLabel.text = displayText
+            }
+        }
+        
+        // Execute the request
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+}
+
+struct SwiftUIAVCaptureVideoPreviewView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIAVCaptureVideoPreviewView {
+        let view = UIAVCaptureVideoPreviewView()
+        view.setModel()
+        view.setupSession()
+        view.setupPreview()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIAVCaptureVideoPreviewView, context: Context) {
+    }
+}
